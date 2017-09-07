e18aea989a0608b461879a86d4837f75af4baa4a
Universe <universe@MacBook-Air.local>
2017-09-07 00:56:16 -0400

    change folder name

--- runall.sh
+++ runall.sh
@@ -1,8 +1,8 @@
 ### train RNN
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/bin/rnn.py
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/rnn.py
 ### get RNN hidden states for testing data
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/bin/rnn_analysis.py
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/rnn_analysis.py
 ### using Gaussian mixture & K means to cluster hidden states
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/bin/gmm_kmeans.py
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans.py
 ### get network tables
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/bin/get_network.py
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/get_network.py
