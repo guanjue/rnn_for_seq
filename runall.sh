77b89a4dd159708b9589a30680842f5a0a972ee2
Universe <universe@MacBook-Air.local>
2017-09-07 16:37:57 -0400

    all pipeline modify

--- runall.sh
+++ runall.sh
@@ -1,20 +1,20 @@
 ### package location ~/rnn_for_seq/
 ### train RNN
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/pred_models/rnn.py
+#time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/pred_models/rnn.py
 ### get RNN hidden states for testing data
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/pred_models_info_extract/rnn_analysis.py
+#time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/pred_models_info_extract/rnn_analysis.py
 ### using Gaussian mixture & K means to cluster hidden states
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/gmm_kmeans.py
+#time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/gmm_kmeans.py
 ### get network tables
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/get_network.py
+#time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/get_network.py
 
 ########################################################
 ###### simulation analysis
 cd /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/simulate_data_vs_SHUFFLE
 ### generate random sequence with 2 motifs motif1 downstream of motif2
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator.py -l 100 -n 10000 -u ACGT -a 03322 -b 30331 -s 2017 -r upstream -o motif1_up_motif2.fa
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator_coverall.py -l 100 -n 10000 -u ACGT -a 03322  -p 1.0 -b 30331  -q 0.6 -s 2017 -r upstream -o motif1_up_motif2.fa
 ### dimer shuffle sequence
-time fasta-dinucleotide-shuffle -f motif1_up_motif2.fa -s 2017 -c 2 > motif1_up_motif2.shuffle.fa
+time fasta-dinucleotide-shuffle -f motif1_up_motif2.fa -s 2017 > motif1_up_motif2.shuffle.fa
 ### fasta2seq
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/fasta2numseq.py -f motif1_up_motif2.fa -o motif1_up_motif2.txt -u ACGT
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/fasta2numseq.py -f motif1_up_motif2.shuffle.fa -o motif1_up_motif2.shuffle.txt -u ACGT
