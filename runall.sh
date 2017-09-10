c0363c5e981b0c145fe7ed00c696ef4a5e39ff39
Universe <universe@MacBook-Air.local>
2017-09-10 00:28:26 -0400

    add motif align

--- runall.sh
+++ runall.sh
@@ -13,18 +13,25 @@
 #cd /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/simulate_data_vs_SHUFFLE_3motif
 ### generate random sequence with 2 motifs motif1 downstream of motif2
 #time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator_coverall.py -l 100 -n 10000 -u ACGT -a 03322  -p 0.95 -b 30331  -q 0.75 -s 2017 -r upstream -o motif1_up_motif2.fa
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator_3motif_coverall.py -l 100 -n 10000 -u ACGT -a 03322 -p 0.75 -b 10132 -q 0.85 -c 33301 -t 0.5 -s 2017 -r upstream -v downstream -o motif1_up_motif2_down_motif3.fa
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator_3motif_coverall.py -l 100 -n 5000 -u ACGT -a 03322 -p 1 -b 10132 -q 1 -c 33301 -t 1 -s 2017 -r upstream -v downstream -o motif1_up_motif2_down_motif3.fa
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/simulation/fasta_generator_3motif_coverall.py -l 100 -n 5000 -u ACGT -a 03322 -p 1 -b 10132 -q 1 -c 33301 -t 1 -s 2017 -r downstream -v upstream -o motif1_up_motif2_down_motif3_noorder.fa
 
 ### dimer shuffle sequence
 time fasta-dinucleotide-shuffle -f motif1_up_motif2_down_motif3.fa -s 2017 > motif1_up_motif2_down_motif3.shuffle.fa
 ### fasta2seq
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/fasta2numseq.py -f motif1_up_motif2_down_motif3.fa -o motif1_up_motif2_down_motif3.txt -u ACGT
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/fasta2numseq.py -f motif1_up_motif2_down_motif3.shuffle.fa -o motif1_up_motif2_down_motif3.shuffle.txt -u ACGT
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/fasta2numseq.py -f motif1_up_motif2_down_motif3_noorder.fa -o motif1_up_motif2_down_motif3_noorder.txt -u ACGT
+
 ### split training data & testing data
-head -6000 motif1_up_motif2_down_motif3.txt > train_pos.txt
-head -6000 motif1_up_motif2_down_motif3.shuffle.txt > train_neg.txt
-tail -n+6001 motif1_up_motif2_down_motif3.txt > test_pos.txt
-tail -n+6001 motif1_up_motif2_down_motif3.shuffle.txt > test_neg.txt
+head -3000 motif1_up_motif2_down_motif3.txt > train_pos.txt
+#head -6000 motif1_up_motif2_down_motif3.shuffle.txt > train_neg.txt
+head -3000 motif1_up_motif2_down_motif3_noorder.txt > train_neg.txt
+
+tail -n+3001 motif1_up_motif2_down_motif3.txt > test_pos.txt
+#tail -n+6001 motif1_up_motif2_down_motif3.shuffle.txt > test_neg.txt
+tail -n+3001 motif1_up_motif2_down_motif3_noorder.txt > test_neg.txt
+
 ### prepare input data (txt2npz)
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/prepare_input/txt2npz.py -a train_pos.txt -b test_pos.txt -c train_neg.txt -d test_neg.txt -e train.npz -f test.npz
 ### train RNN
@@ -34,5 +41,6 @@ time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/r
 ### using Gaussian mixture & K means to cluster hidden states
 time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/gmm_kmeans.py
 ### get network tables
-time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/get_network.py
-
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/get_network_clusterseq.py
+### NW method align sequence
+time python /Volumes/MAC_Data/data/labs/zhang_lab/sequence_prediction/nfyb_fos/rnn_for_seq/bin/gmm_kmeans_network/needleman_wunsch_align.py
