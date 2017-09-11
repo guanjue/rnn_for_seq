e1eb88d3ad6a0f3ce43849a27ea21632734dc464
Universe <universe@MacBook-Air.local>
2017-09-11 02:00:31 -0400

    update

--- bin/gmm_kmeans_network/needleman_wunsch_align.py
+++ bin/gmm_kmeans_network/needleman_wunsch_align.py
@@ -220,7 +220,7 @@ align_motif('network_matrix_thresh_noself.txt', 'network_matrix_thresh_noself_al
 
 k=5
 for i in range(k):
-	align_motif('network_table_pos_kmeans/network_superstate_matrix_thresh_pos_'+str(i)+'.txt', 'network_table_pos_kmeans/network_superstate_matrix_thresh_pos_'+str(i)+'_aligned.txt')
+	align_motif('network_table_pos_kmeans/network_superstate_matrix_thresh_noself_pos_'+str(i)+'.txt', 'network_table_pos_kmeans/network_superstate_matrix_thresh_noself_pos_'+str(i)+'_aligned.txt')
 	align_motif('network_table_pos_kmeans/network_matrix_thresh_noself_pos_'+str(i)+'.txt', 'network_table_pos_kmeans/network_matrix_thresh_noself_pos_'+str(i)+'_aligned.txt')
 
 
